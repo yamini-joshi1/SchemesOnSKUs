758e3887a0f66b8e14c105a2a353bccd78650809
yamini-joshi1 <yaminijoshi010@gmail.com>
2021-04-06 20:12:37 +0530

    Add project files.

---
+++ SchemesOnSKUs/Models/PrimaryCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchemesOnSKUs.Models
+{
+    public class PrimaryCategory
+    {
+        public int Id { get; set; }
+        public string PCatName { get; set; }
+        public string PCatDesc { get; set; }
+    }
+}
